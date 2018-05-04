ab9c00a202bbfcef55d33632a1018888f7d5c0b9
QuirinEcker <quirin.ecker@bajupa.com>
2018-05-04 04:02:01 +0200

    working on the foundaitions

---
+++ js/createCalender.js
@@ -0,0 +1,9 @@
+function runCalender() {
+	let opazity = document.getElementById("startScreenBackground");
+	let opazityInput = document.getElementById("nameEnter");
+	
+	opazity.style.opacity = 0;
+	opazityInput.style.opacity = 0;
+	
+	
+}
